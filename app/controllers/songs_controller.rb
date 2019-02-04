82387eff2ff32705743719cdc0807497c235d7e1
Benjamin Jones <bljones36@gmail.com>
2019-02-04 02:26:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,10 +46,8 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    if params[:song][:artist_name] != ""
-      @artist = Artist.create(name: params[:song][:artist_name])
-      @song.artist = @artist
-    end
+    @artist = Artist.create(name: params[:song][:artist_name])
+    @song.artist = @artist
     @genres = []
     params[:genres].each do |genre_id|
       if Genre.find_by(id: genre_id) != nil
