9c72889a181a6c29fbd92741f0c35deab3148fd0
Benjamin Jones <bljones36@gmail.com>
2019-02-04 03:15:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,7 +53,7 @@ class SongsController < ApplicationController
     end
 
     if params[:genres] != nil
-      binding.pry
+      # binding.pry
       @genres = Genre.find_by(id: params[:genres][0].to_i)
       @song.update(genres: @genres)
       @song.save
